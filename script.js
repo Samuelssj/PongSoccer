ec51b390c79034123fc8eabda11c17af49993571
Samuel Júnior <samuelssj06@gmail.com>
2022-01-25 22:36:05 -0300

    map and players ready

---
+++ script.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas")
+var container = canvas.getContext("2d")
+
+container.fillStyle = "#50c878"
+var jogador1 = container.fillRect(80,260,30,200)
+var jogador2 = container.fillRect(1200,260,30,200)
+var bolinha = container.fillRect(1280/2 -15,720/2,30,30) 
