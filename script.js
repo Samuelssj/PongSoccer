9e023507d3cbb6b575c1da9a82cb7f378db2408a
Samuel Júnior <samuelssj06@gmail.com>
2022-01-26 22:05:09 -0300

    finished collision

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var jogador1 ={
     py: 260,
     tx:30,
     ty:200,
+    dir:0,
 }
 var jogador2 ={
     px: 1200,
@@ -22,7 +23,9 @@ var bolinha ={
     px: 1280/2 -15,
     py: 720/2,
     tx:30,
-    ty:30,
+    ty:30, 
+    dir:8,
+
 }
 
 container.font = "25px Arial" 
@@ -30,8 +33,61 @@ var pts1 = 0
 var pts2 = 0
 
 
+
+
+document.addEventListener("keydown", function(e){
+    if(e.keyCode === 87){
+        jogador1.dir = -10
+    }else if(e.keyCode === 83){
+        jogador1.dir = 10 
+    }
+    
+})
+
+document.addEventListener("keyup", function(e){
+    if(e.keyCode === 87){
+        jogador1.dir = -0
+    }else if(e.keyCode === 83){
+        jogador1.dir = 0 
+    }
+    
+})
+
+
+
+function Move_Player1(){
+
+    if(jogador1.py < 0){
+        jogador1.py =0
+    }else if(jogador1.py >520){
+        jogador1.py =520
+
+    }
+
+
+
+    jogador1.py += jogador1.dir
+}
+
+
+
 function Move_Ball(){
-    bolinha.px +=8
+    bolinha.px += bolinha.dir
+
+}
+
+function Colision_Ball(){
+
+    if (bolinha.py + bolinha.ty >= jogador2.py && bolinha.py <= jogador2.py+jogador2.ty &&
+        bolinha.px >= jogador2.px-jogador2.tx){
+
+        bolinha.dir *= -1
+
+    }else if(bolinha.py + bolinha.ty >= jogador1.py && bolinha.py <= jogador1.py+jogador1.ty &&
+         bolinha.px <= jogador1.px+ jogador1.tx){
+             
+        bolinha.dir *= -1
+    }
 }
 
 function Draw(){
@@ -47,6 +103,8 @@ function Main(){
     container.clearRect(0,0,1280,720)
     Draw()
     Move_Ball()
+    Colision_Ball()
+    Move_Player1()
 
 }
-setInterval(Main, 20)
+setInterval(Main, 10)
